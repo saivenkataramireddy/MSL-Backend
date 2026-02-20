dff15d1232f9790702e8a9b067d8fe0edcbe6d6f
saivenkataramireddy <saivenkataramireddykesara@gmail.com>
2026-02-20 12:34:05 +0530

    port added

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # ─── CORS (allow React dev server) ────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5174"],
+    allow_origins=["http://localhost:3000", "http://localhost:5174","https://medical-science-liasion.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
